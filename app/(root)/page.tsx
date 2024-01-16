541ad6e005745cd779305ffca8f5c568a8cf0a75
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-01-16 11:35:33 +0100

    header temporarily created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      <h1 className='text-4xl'>Evently</h1>
-      <Button>Hello</Button>
-      Home
+      Home page
     </main>
   )
 }
