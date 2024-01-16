ac93cac2e9dd3929ba6ee67ff1a28817b7ede850
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-01-16 11:30:09 +0100

    Header setup started

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Shared Footer
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/shared/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            Shared Header
+        </div>
+    )
+}
+
+export default Header
