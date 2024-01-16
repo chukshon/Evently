443d035ec6603b3b2c54172b67d5842751f1e071
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-01-16 11:25:51 +0100

    project setup, dependecies installation and assets added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className='text-4xl'>Evently</h1>
+      <Button>Hello</Button>
+      Home
+    </main>
+  )
+}
