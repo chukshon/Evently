b32e2669e38207dbe94ee64fddc3c2accb658471
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-01-22 13:24:15 +0100

    Created the createUser function that connects to the database and creates a user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,1 +1,20 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+
+import { connectToDatabase } from "@/lib/database";
+import User from "@/lib/database/models/user.model";
+import { handleError } from "@/lib/utils";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
