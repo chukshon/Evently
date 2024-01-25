ccb69916e59cb772c9388cab085577b5fb88cb0d
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-01-25 12:47:58 +0100

    fully setup the react hook form, added the schema and also created the event title form item

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,15 +1,60 @@
 "use client"
 import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { eventFormSchema } from '@/lib/validator'
+import { eventDefaultValues } from '@/constants'
+
 
 type EventFormProps = {
     userId: string,
     type: "Create" | "Update"
 }
+
 const EventForm = ({ userId, type }: EventFormProps) => {
+    const initialValues = eventDefaultValues
+
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+        resolver: zodResolver(eventFormSchema),
+        defaultValues: initialValues
+    })
+
+    function onSubmit(values: z.infer<typeof eventFormSchema>) {
+        console.log(values)
+    }
     return (
-        <div>
-            Event form
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className='w-full '>
+                                <FormControl>
+                                    <Input placeholder="Event Title" {...field} className='input-field' />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
     )
 }
 
